0f9533dd9278d8ec11a60abaddfe7f7090f71638
Szymon <sz.orlowski95@gmail.com>
2018-11-13 11:39:55 +0100

    Burger Builder

---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import classes from './Modal.css'
+import Backdrop from "../Backdrop/Backdrop";
+
+class Modal extends Component {
+
+  shouldComponentUpdate(nextProps, nexState){
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        {this.props.show ? <div
+          className={classes.Modal}
+          style={{
+            transformed: this.props.show ? 'translateY(0)' : "translateY(-100vh)",
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div> : null}
+      </React.Fragment>
+    )
+  }
+}
+
+export default Modal;
